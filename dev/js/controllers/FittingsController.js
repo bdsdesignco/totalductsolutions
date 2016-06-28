b4af0e1fd6400042c636f548e71c135aaad3d569
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-06-28 18:27:12 -0400

    Added initial modal functionality.

---
+++ dev/js/controllers/FittingsController.js
@@ -0,0 +1,53 @@
+app.controller('FittingsController',['$scope', function($scope) {
+  $scope.fittings = [
+    {
+      source: "",
+      alt: "",
+      desc: "plenty of stuff here"
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    }, {
+      source: "",
+      alt: "",
+      desc: ""
+    },
+  ]
+}]);
