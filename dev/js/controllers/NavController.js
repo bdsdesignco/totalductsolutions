a140971cd8278cc3ef621f2a159dca7a52066524
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-07-18 18:06:23 -0400

    Started implementing email form modal

---
+++ dev/js/controllers/NavController.js
@@ -0,0 +1,30 @@
+app.controller('NavController',['$scope', '$mdDialog', function($scope, $mdDialog) {
+
+
+
+  $scope.showEmailModal = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'views/body/modals/emailmodal.html',
+      targetEvent: ev,
+      clickOutsideToClose: true
+    });
+  };
+
+}]);
+
+DialogController.$inject = ['$scope', '$mdDialog'];
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+};
