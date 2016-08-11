ac48c2a6016ca2583fbca95ff9c1c1756a14839f
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-08-10 23:07:19 -0400

    Added ng-droplet Added comments throughout the code Added separate panel on email modal to allow for uploading files Fixed formatting of fields and styling of inputs Auto tabbing is fully operational

--- dev/js/controllers/EmailController.js
+++ dev/js/controllers/EmailController.js
@@ -1,19 +1,24 @@
 $.autotab.selectFilterByClass = true;
 
-app.controller('EmailController',['$scope', '$mdDialog', function($scope, $mdDialog) {
+app.controller('EmailController',['$scope', '$mdDialog', '$mdBottomSheet', function($scope, $mdDialog, $mdBottomSheet) {
 
   $scope.model = 0;
 
   $scope.initAutotab = function() {
     $(function() {
-      $('.number').autotab('number');
+      // $('.number').autotab('number');
     });
   };
 
-  $scope.phone = {areaCode: '', number1: '', number2: ''};
+  $scope.phone = {areaCode: '', number1: '', number2: '', ext: ''};
 
   setTimeout(function() {
     $.autotab.refresh();
   }, 1);
+
+  // Initializing JS injection
   $scope.initAutotab();
+
+  $scope.image = null;
+  $scope.imageFileName = '';
 }]);
