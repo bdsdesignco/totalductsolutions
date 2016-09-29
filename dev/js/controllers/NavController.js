d14bdc317d8471aff24e639f7232bc38e7374622
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-09-29 00:56:12 -0400

    Redesigned navbar with more links.

--- dev/js/controllers/NavController.js
+++ dev/js/controllers/NavController.js
@@ -1,4 +1,28 @@
 app.controller('NavController',['$scope', '$mdDialog', function($scope, $mdDialog) {
+  $scope.links = [
+    {
+      name: "Services",
+      path: "",
+      click: "",
+      styling: ""
+    }, {
+      name: "About",
+      path: "",
+      click: "",
+      styling: ""
+    }, {
+      name: "Contact",
+      path: "",
+      click: "",
+      styling: ""
+    }, {
+      name: "Order",
+      path: "",
+      click: "showEmailModal($event)",
+      styling: "md-raised"
+    }
+  ];
+
   $scope.showEmailModal = function(ev) {
     $mdDialog.show({
       controller: DialogController,
